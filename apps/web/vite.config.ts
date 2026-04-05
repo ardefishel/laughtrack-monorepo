c853b75bd9425bd12e913f85e5c76bbe30a5887d
ardefishel (rtvcl) <ardefishel@gmail.com>
2026-04-05 20:21:42 +0700

    add nitro to web app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: {
     port: 3001,
-    host: '0.0.0.0',
+    host: '0.0.0.0'
   },
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart(),
-    viteReact(),
-    tailwindcss(),
-  ],
-})
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), viteReact(), tailwindcss()]
+});
